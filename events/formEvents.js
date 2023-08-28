064bf3cbda787cb238bac5e3963628feaa37544f
Joey Ebach <connect@joeyebach.com>
2023-08-28 18:03:04 -0500

    added all almost Amazon functionality

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,3 +1,6 @@
+import { createAuthor, getAuthors, updateAuthor } from '../api/authorData';
+import { showAuthors } from '../pages/authors';
+
 const formEvents = () => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,9 +16,34 @@ const formEvents = () => {
       console.warn(firebaseKey);
     }
 
+    if (e.target.id.includes('submit-author')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      const payload = {
+        email: document.querySelector('#email').value,
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last-name').value,
+        firebaseKey,
+      };
+      updateAuthor(payload).then(() => {
+        getAuthors().then(showAuthors);
+      });
+    }
+
     // FIXME: ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
     if (e.target.id.includes('submit-author')) {
-      console.warn('CLICKED SUBMIT AUTHOR');
+      const payload = {
+        email: document.querySelector('#email').value,
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last_name').value,
+      };
+
+      createAuthor(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateAuthor(patchPayload).then(() => {
+          getAuthors().then(showAuthors);
+        });
+      });
     }
     // FIXME:ADD CLICK EVENT FOR EDITING AN AUTHOR
   });
